b5c149db7c95b8140a36e3e40856ebe55866d2cf
kiritoj <2552744904@qq.com>
2019-05-19 21:59:28 +0800

    第二次提交

---
+++ app/src/main/java/com/example/mifans/eif/Activities/CollectActivity.java
@@ -0,0 +1,98 @@
+package com.example.mifans.eif.Activities;
+
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.example.mifans.eif.R;
+import com.example.mifans.eif.other.RecyclerAdapter;
+import com.example.mifans.eif.other.Songbean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CollectActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    RecyclerView recyclerView;
+    RecyclerAdapter songsAdapter;
+    SwipeRefreshLayout refreshLayout;
+    private List<Songbean> songsList = new ArrayList<Songbean>();
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_collect);
+        initViews();
+
+    }
+    public void initViews(){
+        refreshLayout = findViewById(R.id.mycollect_refresh);
+        recyclerView = findViewById(R.id.mycollect_recycleview);
+        toolbar = findViewById(R.id.tool_bar_c);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.ic_back);
+        }
+        initCollectedNews();
+        songsAdapter = new RecyclerAdapter(songsList,this);
+        recyclerView.setAdapter(songsAdapter);
+        LinearLayoutManager manager = new LinearLayoutManager(CollectActivity.this);
+        recyclerView.setLayoutManager(manager);
+        refreshLayout.setColorSchemeResources(R.color.colorPrimaryDark);
+        refreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                //清除收藏列表，产生新的收藏列表
+                songsList.clear();
+
+                initCollectedNews();
+                songsAdapter.notifyDataSetChanged();
+                refreshLayout.setRefreshing(false);
+                Toast.makeText(CollectActivity.this, "收藏列表已刷新成功", Toast.LENGTH_SHORT).show();
+
+
+            }
+        });
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            default:
+                break;
+        }
+        return true;
+    }
+    public void initCollectedNews(){
+        //从数据库加载已经收藏的新闻
+        MyDataBaseHelper databaseHelper = new MyDataBaseHelper(CollectActivity.this, "User.db", null, 1);
+        SQLiteDatabase database = databaseHelper.getWritableDatabase();
+        Cursor cursor = database.query("Collect",null,null,null,null,null,null);
+        if (cursor.moveToLast()){
+            //从后往前遍历，最后收藏的处于最顶层
+            do {
+                String songid = cursor.getString(cursor.getColumnIndex("songid"));
+                String songname = cursor.getString(cursor.getColumnIndex("songname"));
+                String singer = cursor.getString(cursor.getColumnIndex("singer"));
+                String coverurl = cursor.getString(cursor.getColumnIndex("coverurl"));
+                Songbean news = new Songbean(songname,singer,coverurl,songid);
+                songsList.add(news);
+
+            }while (cursor.moveToPrevious());
+        }
+    }
+}
---
+++ app/src/main/java/com/example/mifans/eif/other/RecyclerAdapter.java
@@ -0,0 +1,76 @@
+package com.example.mifans.eif.other;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.mifans.eif.Activities.MainActivity;
+import com.example.mifans.eif.Activities.MoreActivity;
+import com.example.mifans.eif.R;
+import com.example.mifans.eif.Tools.MyImageLoader;
+
+import org.w3c.dom.Text;
+
+import java.util.List;
+
+public class RecyclerAdapter extends RecyclerView.Adapter<RecyclerAdapter.ViewHolder> {
+    private List<Songbean> songbeans;
+    private Context context;
+    public RecyclerAdapter(List<Songbean> songbeans,Context context) {
+        this.songbeans = songbeans;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, final int i) {
+        View view = LayoutInflater.from(context).inflate(R.layout.itemcollect,viewGroup,false);
+        ViewHolder holder = new ViewHolder(view);
+        holder.cover.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(context,MoreActivity.class);
+                Songbean songbean = songbeans.get(i);
+                intent.putExtra("info",songbean);
+                context.startActivity(intent);
+            }
+        });
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+        Songbean songbean = songbeans.get(i);
+        viewHolder.songname.setText(songbean.getSongName());
+        viewHolder.singername.setText(songbean.getSingerName());
+        MyImageLoader.with(context).into(viewHolder.cover)
+                .placeholder(R.drawable.ic_default_bottom_music_icon)
+                .load(songbean.getCoverUrl());
+    }
+
+    @Override
+    public int getItemCount() {
+        return songbeans.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        View song;
+        ImageView cover;
+        TextView songname;
+        TextView singername;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            song = itemView;
+            cover = itemView.findViewById(R.id.cover);
+            songname = itemView.findViewById(R.id.item_song_name);
+            singername = itemView.findViewById(R.id.item_singer_name);
+        }
+    }
+}
